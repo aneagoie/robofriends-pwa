bfe8eb250d1783d7306de60683011c3d07939ee9
aneagoie <aneagoie1@gmail.com>
2018-03-26 17:09:27 -0500

    performance fix

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,7 +4,9 @@ const SearchBox = ({ searchfield, searchChange }) => {
   console.log('SearchBox');
   return (
     <div className='pa2'>
+      <label for="search"></label>
       <input
+        id='search'
         className='pa3 ba b--green bg-lightest-blue'
         type='search'
         placeholder='search robots'
