86b49dc9f7470d1d788f3fb248e7d8e8d73a79fa
Andrei Neagoie <aneagoie@Andreis-MBP-2.lan>
2022-09-22 13:11:21 -0400

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom'; The new way to import createRoot:
+import { createRoot } from "react-dom/client";
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
@@ -11,20 +12,27 @@ import App from './containers/App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { requestRobots, searchRobots } from './reducers'
 
-import './index.css'; 
+import './index.css';
 
-const logger = createLogger()
+const logger = createLogger() 
 
 const rootReducers = combineReducers({requestRobots, searchRobots})
 
 const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger))
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App/>
-  </Provider>,
-  document.getElementById('root')
+const root = createRoot(document.getElementById('root'));
+root.render(
+<Provider store={store}>
+  <App/>
+</Provider>
 );
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App/>
+//   </Provider>,
+//   document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
