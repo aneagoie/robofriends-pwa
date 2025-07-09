907cf352b30d0181b7e9c1299487b1f81a7c0546
Shashwat Dahal <thuglifescientist2018@gmail.com>
2025-07-09 12:44:07 +0545

    course: complete actions.js test

--- src/actions.test.js
+++ src/actions.test.js
@@ -7,25 +7,26 @@ import {
    } from './constants';
 
 import configureMockStore from 'redux-mock-store';
-import thunkMiddleware from 'redux-thunk';
+import { thunk as thunkMiddleware } from 'redux-thunk';
 
-const mockStore  = configureMockStore([thunkMiddleware])
+const mockStore = configureMockStore([thunkMiddleware]);
+
 it('should create an action to search robots', ()=> {
     const text = 'wooo';
     const expectedAction = {
         type: CHANGE_SEARCHFIELD,
         payload: text
-    }
-    expect(actions.setSearchField(text)).toEqual(expectedAction)
-})
+    };
+    expect(actions.setSearchField(text)).toEqual(expectedAction);
+});
 
 it("handles requesting robots API", () => {
     const store = mockStore();
-    store.dispatch(actions.requestRobots())
-    const action = store.getActions()
-    console.log('action', action)
+    store.dispatch(actions.requestRobots());
+    const action = store.getActions();
+    console.log('action', action);
     const expectedAction = {
         type: REQUEST_ROBOTS_PENDING,
-    }
-    expect(actions.requestRobots()).toEqual(expectedAction)
+    };
+    expect(action[0]).toEqual(expectedAction);
 })
